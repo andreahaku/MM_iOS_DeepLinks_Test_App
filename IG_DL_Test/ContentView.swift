b6b7988ab7c80adea1e9852945a6628db2cb7edc
Andrea Salvatore <andrea.salvatore@consensys.net>
2022-05-12 11:22:16 +0200

    Initial Commit

---
+++ IG_DL_Test/ContentView.swift
@@ -0,0 +1,21 @@
+//
+//  ContentView.swift
+//  IG_DL_Test
+//
+//  Created by Andrea Salvatore on 12/05/2022.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        Text("Hello, world!")
+            .padding()
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
---
+++ IG_DL_Test/IG_DL_TestApp.swift
@@ -0,0 +1,17 @@
+//
+//  IG_DL_TestApp.swift
+//  IG_DL_Test
+//
+//  Created by Andrea Salvatore on 12/05/2022.
+//
+
+import SwiftUI
+
+@main
+struct IG_DL_TestApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
