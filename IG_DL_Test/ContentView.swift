4238e3b4abc75cfe73ab272d96b7dea360b6d128
Andrea Salvatore <andrea.salvatore@consensys.net>
2022-05-12 18:46:02 +0200

    first commit

--- IG_DL_Test/ContentView.swift
+++ IG_DL_Test/ContentView.swift
@@ -6,11 +6,28 @@
 //
 
 import SwiftUI
+import UIKit
+import WebKit
 
 struct ContentView: View {
+    let url = URL(string: "https://metamask.app.link/google.com")!
+    let url2 = URL(string: "dapp://google.com")!
+
     var body: some View {
-        Text("Hello, world!")
-            .padding()
+        Button("Open Deep Link 1") {
+            UIApplication.shared.open(url)
+        }
+        
+        VStack(alignment: .leading) {
+            Link("Open Deep Link 2", destination: url)
+                .foregroundColor(.blue)
+        }
+
+        VStack(alignment: .leading) {
+            Link("Open Deep Link 3", destination: url2)
+                .foregroundColor(.blue)
+        }
+
     }
 }
 
